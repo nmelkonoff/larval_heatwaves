322ff0cd4a48fbe03d5c9104c74f4f3d873ab703
Natalie <nmelkonoff@dbg.org>
2024-02-13 16:41:21 -0700

    Added ANOVAs for fwl

--- anova.R
+++ anova.R
@@ -112,7 +112,7 @@ p_pupal_days <- ggplot(df_pupal_days, aes(x=hostplant, y=pupal_days, fill=treatm
   geom_errorbar(aes(ymin=pupal_days-sd, ymax=pupal_days+sd), width=.2,
                 position=position_dodge(.9))
 
-p_pupal_days + scale_fill_manual(values = c("darkblue", "darkgreen", "purple", "red", "yellow"))
+p_pupal_days + scale_fill_manual(values = c("darkblue", "darkgreen", "red", "yellow"))
 
 
 #pupal_mass
@@ -138,7 +138,7 @@ p_pupal_mass <- ggplot(df_pupal_mass, aes(x=hostplant, y=pupal_mass, fill=treatm
   geom_errorbar(aes(ymin=pupal_mass-sd, ymax=pupal_mass+sd), width=.2,
                 position=position_dodge(.9))
 
-p_pupal_mass + scale_fill_manual(values = c("darkblue", "darkgreen", "purple", "red", "yellow"))
+p_pupal_mass + scale_fill_manual(values = c("darkblue", "darkgreen", "red", "yellow"))
 
 
 #day10_mass
@@ -164,7 +164,7 @@ p_day10_mass <- ggplot(df_day10_mass, aes(x=hostplant, y=day10_mass, fill=treatm
   geom_errorbar(aes(ymin=day10_mass-sd, ymax=day10_mass+sd), width=.2,
                 position=position_dodge(.9))
 
-p_day10_mass + scale_fill_manual(values = c("darkblue", "darkgreen", "purple", "red", "yellow"))
+p_day10_mass + scale_fill_manual(values = c("darkblue", "darkgreen", "red", "yellow"))
 
 
 #larval_days
@@ -190,7 +190,7 @@ p_larval_days <- ggplot(df_larval_days, aes(x=hostplant, y=larval_days, fill=tre
   geom_errorbar(aes(ymin=larval_days-sd, ymax=larval_days+sd), width=.2,
                 position=position_dodge(.9))
 
-p_larval_days + scale_fill_manual(values = c("darkblue", "darkgreen", "purple", "red", "yellow"))
+p_larval_days + scale_fill_manual(values = c("darkblue", "darkgreen", "red", "yellow"))
 
 
 #re-organizing ANOVAs
@@ -198,22 +198,55 @@ p_larval_days + scale_fill_manual(values = c("darkblue", "darkgreen", "purple", 
 #is fwl dependent on hostplant spp and/or treatment?
 model <- aov(r2_adult$fwl ~ r2_adult$hostplant * r2_adult$treatment, 
              data = r2_adult)
-####>>
 
+#pull out hostplant data by species
+alin_data <- r2_adult[which(r2_adult$hostplant == "alin"), names(r2_adult)
+                      %in% c("larval_days", "day10_mass", "pupal_mass", "pupal_days", 
+                             "fwl", "treatment", "plant_heat", "cat_heat")]
 
+aang_data <- r2_adult[which(r2_adult$hostplant == "aang"), names(r2_adult)
+                      %in% c("larval_days", "day10_mass", "pupal_mass", "pupal_days", 
+                             "fwl", "treatment", "plant_heat", "cat_heat")]
+
+asubu_data <- r2_adult[which(r2_adult$hostplant == "asubu"), names(r2_adult)
+                       %in% c("larval_days", "day10_mass", "pupal_mass", "pupal_days", 
+                              "fwl", "treatment", "plant_heat", "cat_heat")]
+
+####ANOVAS for fwl
 #one 2-way ANOVA where there are 4 treatments
-model <- aov(r2_adult$fwl ~ r2_adult$hostplant * r2_adult$treatment, 
+model1_fwl <- aov(r2_adult$fwl ~ r2_adult$hostplant * r2_adult$treatment, 
              data = r2_adult)
 
+summary(model1_fwl)
+
 #three one-way ANOVAs where there are 4 treatments
-model <- aov(r2_adult$fwl ~ r2_adult$treatment, data = r2_adult)
+model_alin_fwl <- aov(alin_data$fwl ~ alin_data$treatment, data = alin_data)
+model_aang_fwl <- aov(aang_data$fwl ~ aang_data$treatment, data = aang_data)
+model_asubu_fwl <- aov(asubu_data$fwl ~ asubu_data$treatment, data = asubu_data)
+
+summary(model_alin_fwl)
+summary(model_aang_fwl)
+summary(model_asubu_fwl)
 
 #three two-way ANOVAs where there are 2 treatments (plant heat and cat heat)
+model_alin_2way_fwl <- aov(alin_data$fwl ~ alin_data$plant_heat * alin_data$cat_heat,
+                           data = alin_data)
+
+model_aang_2way_fwl <- aov(aang_data$fwl ~ aang_data$plant_heat * aang_data$cat_heat,
+                           data = aang_data)
+
+model_asubu_2way_fwl <- aov(asubu_data$fwl ~ asubu_data$plant_heat * asubu_data$cat_heat,
+                            data = asubu_data)
+
+summary(model_alin_2way_fwl)
+summary(model_aang_2way_fwl)
+summary(model_asubu_2way_fwl)
 
 #one 3-way ANOVA with everything
-model <- aov(r2_adult$fwl ~ cat_heat ~ plant_heat ~ hostplant)
+model_3way_everything_fwl <- aov(r2_adult$fwl ~ r2_adult$cat_heat * r2_adult$plant_heat *
+                                   r2_adult$hostplant, data = r2_adult)
 
-
+summary(model_3way_everything_fwl)
 
 # 
 # #more box plots
